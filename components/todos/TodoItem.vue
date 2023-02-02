2a5142ef4a1c30417f3ccd0a9200382310091c55
MogliCodes <moglicodes@gmail.com>
2023-02-02 07:21:09 +0100

    feat(TodoItem): add basic component

---
+++ components/todos/TodoItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex justify-between  bg-white bg-opacity-75 py-8 px-6">
+    <span class="text-lg">Learn 10 spanish verbs</span>
+    <div class="">
+      <slot name="status"></slot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+// const props = defineProps<{}>()
+
+</script>
