c4907010de02d8f0377992a9a972446485c2b65a
MogliCodes <moglicodes@gmail.com>
2023-02-02 06:44:27 +0100

    fix(BaseButton): rename props

--- components/base/BaseButton.vue
+++ components/base/BaseButton.vue
@@ -1,6 +1,6 @@
 <template>
-    <button class="bg-mc-blue text-white font-display font-bold rounded-full inline-block py-1 px-3"> 
-        {{ text }}
+    <button class="bg-mc-blue text-white font-display font-bold rounded-full inline-block py-1 px-3">
+        {{ props.text }}
     </button>
 </template>
 
@@ -10,6 +10,6 @@ interface Props {
     text: String
 }
 
-const { text } = defineProps<Props>()
+const props = defineProps<Props>()
 
 </script>
