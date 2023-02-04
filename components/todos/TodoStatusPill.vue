fedeff3be5ca8979ae0cc88893d59abacdd9e864
MogliCodes <moglicodes@gmail.com>
2023-02-04 20:02:57 +0100

    feat(TodoStatusPill): add conditional pill color

--- components/todos/TodoStatusPill.vue
+++ components/todos/TodoStatusPill.vue
@@ -1,3 +1,19 @@
 <template>
-  <span class="rounded-full bg-green-500 px-3 py-1.5 text-sm font-bold text-green-200">To do</span>
+  <span :class="pillColorClass" class="rounded-full px-3 py-1.5 text-sm font-bold">{{ $attrs.text }}</span>
 </template>
+
+<script setup lang="ts">
+const attrs = useAttrs();
+
+const pillColorClass = computed<string>(() =>{
+  switch(attrs.text) {
+    case 'todo':
+      return 'bg-rose-400 text-white'
+    case 'doing':
+      return 'bg-mc-lightblue text-white'
+    case 'done':
+      return 'bg-mc-fern text-white'
+  }
+})
+
+</script>
