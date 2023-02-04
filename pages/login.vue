8d71b2add1f9454b931771e39213dde10b85001a
MogliCodes <moglicodes@gmail.com>
2023-02-04 20:03:41 +0100

    feat(Login): create basic page

---
+++ pages/login.vue
@@ -0,0 +1,26 @@
+<template>
+  <main class="pt-24">
+    <div class="container mx-auto flex flex-col items-center justify-center">
+      <h1 class="text-4xl font-bold">Hi there!</h1>
+      <p class="mb-8">Please login to use motodo.app</p>
+      <BaseButton :is-large="true" text="Login with Google" @click="login('google', )" />
+    </div>
+  </main>
+</template>
+
+<script setup lang="ts">
+const client = useSupabaseAuthClient();
+const router = useRouter();
+
+async function login(provider: "github" | "google" | "gitlab" | "bitbucket") {
+  const { error } = await client.auth.signInWithOAuth({
+    provider,
+    options: {
+      redirectTo: "/",
+    },
+  });
+  if (error) {
+    return alert("Something went wrong !");
+  }
+}
+</script>
