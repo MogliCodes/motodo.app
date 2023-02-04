7ec0fc371629b739994ad1d4cabbb1e748fad4f3
MogliCodes <moglicodes@gmail.com>
2023-02-04 20:02:15 +0100

    feat(TodoCreatePanel): create basic component

---
+++ components/todos/TodoCreatePanel.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <BaseHeadline text="Add a new todo" />
+    <div class="mb-24 flex gap-8">
+      <BaseInput />
+      <BaseButton @click="createTodo(todoInput)" class="w-2/12 text-2xl" :is-large="true" text="Add todo" />
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import {createTodo} from "~/composables/todos";
+
+const todoInput = ref()
+
+</script>
