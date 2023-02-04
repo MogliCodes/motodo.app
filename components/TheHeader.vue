61caae1f380a5c4d1911ad5403d2f499b8749b76
MogliCodes <moglicodes@gmail.com>
2023-02-04 20:03:20 +0100

    feat(TheHeader): add logout button

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,8 +1,16 @@
 <template>
     <header class="py-2">
-        <div class="container mx auto flex justify-between">
+        <div class="mx auto container flex justify-between">
             <h1 class="font-bold">motodo.app</h1>
-            <BaseButton is-small text="Login" />
+            <div class="flex gap-4">
+              <BaseButton v-if="!user" is-small text="Login" />
+              <BaseButton v-else is-small text="Logout" @click="client.auth.signOut()" />
+            </div>
         </div>
     </header>
 </template>
+
+<script setup lang="ts">
+const user = useSupabaseUser();
+const client = useSupabaseAuthClient();
+</script>
