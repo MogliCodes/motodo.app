85325072c4147b3088e8ffbb25a4d4347c3a3b18
MogliCodes <moglicodes@gmail.com>
2023-02-04 20:02:05 +0100

    feat(TodoListPanel): create basic component

---
+++ components/todos/TodoListPanel.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="shadow-lg">
+    <BaseHeadline text="Your todos" />
+    <div class="flex flex-col gap-0.5 rounded-t-2xl rounded-b-2xl overflow-hidden">
+      <TodoItem v-for="todo in $attrs.todos" v-bind="todo" :key="todo.id" />
+    </div>
+  </section>
+</template>
+<script setup lang="ts">
+import TodoItem from "~/components/todos/TodoItem.vue";
+</script>
