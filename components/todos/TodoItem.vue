93e9a214de3a95d18ce35142e362b6844f6e562c
MogliCodes <moglicodes@gmail.com>
2023-02-12 15:08:35 +0100

    feat(TodoItem): add todo classes

--- components/todos/TodoItem.vue
+++ components/todos/TodoItem.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex justify-between bg-white bg-opacity-50 py-8 px-6">
     <div>
-      <span class="text-xl font-bold">{{  $attrs.title }}</span><br>
-      <span>{{ $attrs.description }}</span>
+      <span :class="todoClasses" class="text-xl font-bold">{{  $attrs.title }}</span><br>
+      <span :class="todoClasses" >{{ $attrs.description }}</span>
     </div>
     <div class="">
       <TodoStatusPill :text="$attrs.status" />
@@ -13,6 +13,15 @@
 <script setup lang="ts">
 
 // const props = defineProps<{}>()
+const attrs = useAttrs();
 
 import TodoStatusPill from "~/components/todos/TodoStatusPill.vue";
+
+const todoClasses = computed(() => {
+  switch(attrs.status) {
+    case 'done':
+      return 'line-through'
+  }
+})
+
 </script>
